ca7c538908d69fe518e9290dc39db8d93b06db25
RayMa <88625717+RayWangyangMa@users.noreply.github.com>
2023-01-12 15:35:41 -0600

    finalize the project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ The Brain Boost {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;

8039b63968453289484d4a35c1b1eed1b28aeeee
RayMa <88625717+RayWangyangMa@users.noreply.github.com>
2023-02-09 19:35:34 -0600

    reformated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Footer() {
-  const year = new Date().getFullYear();
-  return (
-    <footer>
-      <p>Copyright ⓒ The Brain Boost {year}</p>
-    </footer>
-  );
+	const year = new Date().getFullYear();
+	return (
+		<footer>
+			<p>Copyright ⓒ The Brain Boost {year}</p>
+		</footer>
+	);
 }
 
 export default Footer;
